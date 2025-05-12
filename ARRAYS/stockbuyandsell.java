430ef95ca4a87eb77efec5ef677d29d42b3ecfae
syed-shoab <syedshoab1100@gmail.com>
2025-05-12 10:29:22 +0530

    Best time to buy and sell stocks problem

---
+++ ARRAYS/stockbuyandsell.java
@@ -0,0 +1,24 @@
+package ARRAYS;
+
+public class stockbuyandsell {
+    public static int profitCalc(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0 ;
+
+        for(int i=0;i<prices.length;i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+            }
+            else{
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] ={7,1,5,3,6,4};
+        int profit = profitCalc(prices);
+        System.out.println(profit);
+    }
+}
